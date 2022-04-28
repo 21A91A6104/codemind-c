f6761d7c9f4aa25a96c7edee9d2160962d1e68ae
21A91A6104 <98638898+21A91A6104@users.noreply.github.com>
2022-04-28 18:39:41 +0530

    #include<stdio.h> int main() {     char s[100];     int i;     int sum=0;     int x;     scanf("%[^ ]s",s);     for(i=0;s[i]!=NULL;i++)     {         if((s[i]>='0')&&(s[i]<='9'))         sum=sum+(s[i]-'0');     }     printf("%d",sum);     return 0; }

---
+++ Numbers_in_String.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+    char s[100];
+    int i;
+    int sum=0;
+    int x;
+    scanf("%[^
+]s",s);
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if((s[i]>='0')&&(s[i]<='9'))
+        sum=sum+(s[i]-'0');
+    }
+    printf("%d",sum);
+    return 0;
+}
