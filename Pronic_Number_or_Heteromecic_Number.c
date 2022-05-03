dd7e0b9ae85961b805a24cd2961651844ecf6c50
21A91A6104 <98638898+21A91A6104@users.noreply.github.com>
2022-05-03 09:13:41 +0530

    #include<stdio.h> int main() {     int n,i,count=0;     scanf("%d",&n);     for(i=1;i<n;i++)     {         if(i*(i+1)==n)         count++;     }     if(count>0)     printf("YES");     else     printf("NO"); }

---
+++ Pronic_Number_or_Heteromecic_Number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,count=0;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        if(i*(i+1)==n)
+        count++;
+    }
+    if(count>0)
+    printf("YES");
+    else
+    printf("NO");
+}
