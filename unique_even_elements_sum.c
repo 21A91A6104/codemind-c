78b6dfe4a0ed2139abb47d7d533c2c79ada30f69
21A91A6104 <98638898+21A91A6104@users.noreply.github.com>
2022-06-13 11:28:05 +0530

    #include<stdio.h> #include<math.h> int main() {     int i,j,n,m=0,count=0;     int avg;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         count=0;         for(j=i+1;j<n;j++)         {             if(arr[i]==arr[j])             count++;         }        if(count==0&&arr[i]%2==0)        {            m=m+arr[i];        }     }     printf("%d",m);     return 0; }

---
+++ unique_even_elements_sum.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i,j,n,m=0,count=0;
+    int avg;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        count=0;
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            count++;
+        }
+       if(count==0&&arr[i]%2==0)
+       {
+           m=m+arr[i];
+       }
+    }
+    printf("%d",m);
+    return 0;
+}
