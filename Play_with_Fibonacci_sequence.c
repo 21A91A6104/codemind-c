65b040e11073cec950c79747842ee2b3cc6cc9d0
21A91A6104 <98638898+21A91A6104@users.noreply.github.com>
2022-06-30 20:58:44 +0530

    #include<stdio.h> int main() {     int a=0,b=1,c,n,i;     scanf("%d",&n);     printf("%d ",a);     printf("%d ",b);     for(i=3;i<=n;i++)     {         c=a+b;         printf("%d ",c);         a=b;         b=c;     } }

---
+++ Play_with_Fibonacci_sequence.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=0,b=1,c,n,i;
+    scanf("%d",&n);
+    printf("%d ",a);
+    printf("%d ",b);
+    for(i=3;i<=n;i++)
+    {
+        c=a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
+    }
+}
