d530a963a82b3e928382892786639a0dfdf8eff3
21A91A6104 <98638898+21A91A6104@users.noreply.github.com>
2022-07-20 10:28:10 +0530

    #include<stdio.h> int main() {     int n,d,t,i,j,k,c=0,r=0;     scanf("%d",&n);     for(i=n+1;i<n+10000;i++)     {         c=0;         t=i;         r=0;         while(t!=0)         {             d=t%10;             r=r*10+d;             t=t/10;         }         if(r==i)         {             for(j=2;j<=i/2;j++)             {                 if(i%j==0)                 {                     c++;                 }             }             if(c==0)             {                 printf("%d",i);                 break;             }         }     } }

---
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,d,t,i,j,k,c=0,r=0;
+    scanf("%d",&n);
+    for(i=n+1;i<n+10000;i++)
+    {
+        c=0;
+        t=i;
+        r=0;
+        while(t!=0)
+        {
+            d=t%10;
+            r=r*10+d;
+            t=t/10;
+        }
+        if(r==i)
+        {
+            for(j=2;j<=i/2;j++)
+            {
+                if(i%j==0)
+                {
+                    c++;
+                }
+            }
+            if(c==0)
+            {
+                printf("%d",i);
+                break;
+            }
+        }
+    }
+}
