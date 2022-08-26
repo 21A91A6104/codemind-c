eb14fc0bb013cf1a48c951bee780e64145eb7569
21A91A6104 <98638898+21A91A6104@users.noreply.github.com>
2022-08-26 19:05:25 +0530

    #include<stdio.h> int main() {     int n,even=0,odd=0,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             even++;         }         else         {             odd++;         }     }     printf("%d %d ",even,odd);     return 0; }

---
+++ Count.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,even=0,odd=0,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+    }
+    printf("%d %d ",even,odd);
+    return 0;
+}
